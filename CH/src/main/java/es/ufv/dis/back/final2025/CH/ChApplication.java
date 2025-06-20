98e3fdd36614b533e012eab899e273155359c516
clauhuerta <claudiahr02@icloud.com>
2025-06-20 19:49:19 +0200

    2.Carpetas front y back creadas

---
+++ CH/src/main/java/es/ufv/dis/back/final2025/CH/ChApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.final2025.CH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChApplication.class, args);
+	}
+
+}
---
+++ CH/src/test/java/es/ufv/dis/back/final2025/CH/ChApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.final2025.CH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
